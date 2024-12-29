97607d56b1f49e7e2e4449aca5f84d2c072041b6
orazi-paolo <orazi.paolo10@gmail.com>
2024-12-29 16:32:51 +0100

    first commit

---
+++ csharp-number-es3/Program.cs
@@ -0,0 +1,15 @@
+﻿// dichiaro la variabile age di tipo intero
+int age;
+// chiedo all'utente di inserire la sua età
+Console.WriteLine("Inserisci la tua età:");
+// leggo l'input dell'utente e lo converto in intero
+int.TryParse(Console.ReadLine(), out age);
+// se ha meno di 18 anni
+if (age < 18)
+{
+    Console.WriteLine("Sei troppo giovane per accedere a questo sito web.");
+}
+else // altrimenti
+{
+    Console.WriteLine("Benvenuto nel sito web!");
+}
